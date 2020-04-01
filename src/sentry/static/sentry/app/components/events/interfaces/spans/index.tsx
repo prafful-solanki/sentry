2973223d4b22cf57d521bdbc3d9824f696539cf1
Alberto Leal <mail4alberto@gmail.com>
2020-04-01 13:18:10 -0400

    fetch error events associated with this transaction

--- src/sentry/static/sentry/app/components/events/interfaces/spans/index.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import * as ReactRouter from 'react-router';
 
 import {t} from 'app/locale';
 import SearchBar from 'app/components/searchBar';
@@ -8,16 +9,17 @@ import SentryTypes from 'app/sentryTypes';
 import {Panel} from 'app/components/panels';
 import space from 'app/styles/space';
 import EventView from 'app/utils/discover/eventView';
+import EventsV2 from 'app/utils/discover/eventsv2';
 
 import {SentryTransactionEvent, ParsedTraceType} from './types';
-import {parseTrace} from './utils';
+import {parseTrace, getTraceDateTimeRange} from './utils';
 import TraceView from './traceView';
 
 type Props = {
   orgId: string;
   event: SentryTransactionEvent;
   eventView: EventView;
-};
+} & ReactRouter.WithRouterProps;
 
 type State = {
   parsedTrace: ParsedTraceType;
@@ -49,7 +51,34 @@ class SpansInterface extends React.Component<Props, State> {
   };
 
   render() {
-    const {event, orgId, eventView} = this.props;
+    const {event, orgId, eventView, location} = this.props;
+    const {parsedTrace} = this.state;
+
+    // construct discover query to fetch error events associated with this transaction
+
+    const {start, end} = getTraceDateTimeRange({
+      start: parsedTrace.traceStartTimestamp,
+      end: parsedTrace.traceEndTimestamp,
+    });
+
+    const traceErrorsEventView = EventView.fromSavedQuery({
+      id: undefined,
+      name: `Error events`,
+      fields: [
+        'title',
+        'project',
+        'timestamp',
+        'trace',
+        'trace.span',
+        'trace.parent_span',
+      ],
+      orderby: '-timestamp',
+      query: `event.type:error trace:${parsedTrace.traceID}`,
+      projects: [],
+      version: 2,
+      start,
+      end,
+    });
 
     return (
       <div>
@@ -59,13 +88,24 @@ class SpansInterface extends React.Component<Props, State> {
           placeholder={t('Search for spans')}
           onSearch={this.handleSpanFilter}
         />
+        <EventsV2 location={location} eventView={traceErrorsEventView} orgSlug={orgId}>
+          {({isLoading, tableData}) => {
+            if (isLoading) {
+              return null;
+            }
+
+            console.log('tableData', tableData);
+
+            return <div>hello</div>;
+          }}
+        </EventsV2>
         <Panel>
           <TraceView
             event={event}
             searchQuery={this.state.searchQuery}
             orgId={orgId}
             eventView={eventView}
-            parsedTrace={this.state.parsedTrace}
+            parsedTrace={parsedTrace}
           />
         </Panel>
       </div>
@@ -77,4 +117,4 @@ const StyledSearchBar = styled(SearchBar)`
   margin-bottom: ${space(1)};
 `;
 
-export default SpansInterface;
+export default ReactRouter.withRouter(SpansInterface);
